28e72abdfa5cc78ab763d89b1a47f22cc5daf4dd
Cole Hanson <Antagoniiise@GMail.com>
2025-02-07 09:44:52 -0600

    Create musicDataExample.js

---
+++ mediaFiles/songs/musicDataExample.js
@@ -0,0 +1,27 @@
+export const musicLibrary = [
+    {
+      artist: "Britney Spears",
+      id: "5923287001",
+      genre: "Ppp",
+      similar: ["Ariana Grande", "Selena Gomez", "Avril Lavigne", "Taylor Swift", "Adele", "Rihanna", "Daya", "Lady GaGa", "Demi Lovato", "Paramore", "Pink", "Katy Perry", "Flume"],
+      albums: [
+        {
+          album: "..Baby One More Time",
+          songs: [
+            {
+              id: "5000101",
+              title: "...Baby One More Time",
+              downloadPath: "https://mybeats.cloud/mediaFiles/songs/BabyOneMoreTime.mp3",
+              duration: "3:30",
+            },// More Songs
+          ],
+        },
+        //  More Albums
+      ],
+    }, // More Artists
+  ];
+  
+
+  /**
+   *  THIS IS AN EXAMPLE OF MY MUSIC DATA STRUCTURE
+   */
